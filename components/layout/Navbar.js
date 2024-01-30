516f342f121a123108441df9eaa2b03542383cef
Ali Lotfi <lotfi26.ali@gmail.com>
2024-01-30 17:53:38 +0330

    authentication added to navbar

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -1,23 +1,34 @@
-// import { signOut } from '@/auth';
+import { auth } from '@/auth';
+import { signOut } from '@/auth';
 
-// ui
 import { Button } from '@/components/ui/button';
+import { buttonVariants } from '@/components/ui/button';
+import Link from 'next/link';
 
-export default function Navbar() {
+export default async function Navbar() {
+  const session = await auth();
+  const isLoggedIn = session?.user.email ? true : false;
+
   return (
     <nav className='flex justify-between items-center py-8 px-12 relative'>
       <div className='font-bold text-2xl'>Wallaxy</div>
       <div>
-        <form
-        // action={async () => {
-        //   'use server';
-        //   await signOut();
-        // }}
-        >
-          <Button variant='outline' type='submit'>
-            Log out
-          </Button>
-        </form>
+        {isLoggedIn ? (
+          <form
+            action={async () => {
+              'use server';
+              await signOut();
+            }}
+          >
+            <Button variant='outline' type='submit'>
+              Log out
+            </Button>
+          </form>
+        ) : (
+          <Link href='/login' className={buttonVariants()}>
+            Login
+          </Link>
+        )}
       </div>
       <div className='devider w-[90%] h-[1px] bg-white opacity-10 absolute bottom-0 left-1/2 -translate-x-1/2'></div>
     </nav>
