104722e7c6575197120117cfa560d09392e87a96
Ali Lotfi <lotfi26.ali@gmail.com>
2024-01-30 17:53:54 +0330

    redirecting user after successful login

--- auth.config.ts
+++ auth.config.ts
@@ -8,9 +8,12 @@ export const authConfig = {
         authorized({ auth, request: { nextUrl } }) {
             const isLoggedIn = !!auth?.user;
             const isOnOrderPage = nextUrl.pathname.startsWith('/order');
+            const isOnLoginPage = nextUrl.pathname.startsWith('/login');
             if (isOnOrderPage) {
                 if (isLoggedIn) return true;
                 return false; // Redirect unauthenticated users to login page
+            } else if (isOnLoginPage) {
+                if (isLoggedIn) return Response.redirect(new URL('/', nextUrl));
             }
             return true;
         },
