4f9bc882e4e47c9b017d84b040c6d52d7708b5f2
Ali Lotfi <lotfi26.ali@gmail.com>
2024-02-01 12:09:35 +0330

    typo fix

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className='py-8 px-12 relative flex items-center justify-between'>
       <div className='devider w-[90%] h-[1px] bg-white opacity-10 absolute top-0 left-1/2 -translate-x-1/2'></div>
       <p className='copyright text-sm opacity-70'>
-        2024 All rights reserved for Wallaxy
+        2024 All rights reserved for Fjallraven
       </p>
       <ul className='links flex items-center gap-12 text-sm'>
         <li>Shop</li>
