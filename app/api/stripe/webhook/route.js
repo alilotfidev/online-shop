1548b9d8dde7db899c48f7b9c4c3ff332f2744f6
Ali Lotfi <lotfi26.ali@gmail.com>
2024-08-13 18:06:48 +0330

    testing the webhook

--- app/api/stripe/webhook/route.js
+++ app/api/stripe/webhook/route.js
@@ -6,60 +6,54 @@ import { getOrder, setOrderPaymentStatus } from '@/lib/contentful';
 import { headers } from 'next/headers';
 
 export async function POST(req) {
+  const rawBody = await req.text();
+  const signature = headers().get('stripe-signature');
+  let event;
+
   try {
-    const rawBody = await req.text();
-    const signature = headers().get('stripe-signature');
-
-    console.log({ rawBody, signature, endpointSecret });
-
-    const event = stripe.webhooks.constructEvent(
-      rawBody,
-      signature,
-      endpointSecret
-    );
+    event = stripe.webhooks.constructEvent(rawBody, signature, endpointSecret);
     console.log({ event });
-
-    // Handle the event based on its type (e.g., charge.succeeded)
-    switch (event.type) {
-      case 'charge.succeeded':
-        const succeededCharge = event.data.object;
-        // send email for order completed event (to user and ourselves)
-        console.log('Payment successful:', succeededCharge);
-        // updating the database
-        const succeededOrderId = succeededCharge?.metadata?.order_id || null;
-
-        if (succeededOrderId) {
-          const order = await getOrder(succeededOrderId);
-          const entryId = order?.items[0]?.sys.id || null;
-          if (entryId) {
-            setOrderPaymentStatus(entryId, true);
-          }
-        }
-
-        return NextResponse.json({ message: 'success' }, { status: 200 });
-        break;
-      case 'charge.failed':
-        const failedCharge = event.data.object;
-        // send email for order completed event (to user and ourselves)
-        console.log('Payment successful:', failedCharge);
-        // updating the database
-        const failedOrderId = failedCharge?.metadata?.order_id || null;
-        if (failedOrderId) {
-          const order = await getOrder(failedOrderId);
-          const entryId = order?.items[0]?.sys.id || null;
-          if (entryId) {
-            setOrderPaymentStatus(entryId, false);
-          }
-        }
-        return NextResponse.json({ message: 'success' }, { status: 200 });
-        break;
-      default:
-        console.warn(`Unhandled event type: ${event.type}`);
-    }
   } catch (err) {
     console.error('Webhook error:', err.message);
-    return NextResponse.json({ message: 'error' }, { status: 400 });
+    return NextResponse.json({ message: 'signature error' }, { status: 400 });
   }
+  switch (event.type) {
+    case 'charge.succeeded':
+      const succeededCharge = event.data.object;
+      // send email for order completed event (to user and ourselves)
+      console.log('Payment successful:', succeededCharge);
+      // updating the database
+      const succeededOrderId = succeededCharge?.metadata?.order_id || null;
+
+      if (succeededOrderId) {
+        const order = await getOrder(succeededOrderId);
+        const entryId = order?.items[0]?.sys.id || null;
+        if (entryId) {
+          setOrderPaymentStatus(entryId, true);
+        }
+      }
+
+      return NextResponse.json({ message: 'success' }, { status: 200 });
+      break;
+    case 'charge.failed':
+      const failedCharge = event.data.object;
+      // send email for order completed event (to user and ourselves)
+      console.log('Payment successful:', failedCharge);
+      // updating the database
+      const failedOrderId = failedCharge?.metadata?.order_id || null;
+      if (failedOrderId) {
+        const order = await getOrder(failedOrderId);
+        const entryId = order?.items[0]?.sys.id || null;
+        if (entryId) {
+          setOrderPaymentStatus(entryId, false);
+        }
+      }
+      return NextResponse.json({ message: 'success' }, { status: 200 });
+      break;
+    default:
+      console.warn(`Unhandled event type: ${event.type}`);
+  }
+
   return new NextResponse('RESPONSE EXECUTE', {
     status: 200,
   });
